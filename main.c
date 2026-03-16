06ae75910dc4d90b0c14c68f2df4251211ac4354
ks2330 <110685702+ks2330@users.noreply.github.com>
2026-03-16 13:53:45 +0000

    Fix with main with updated Hardware Abstraction Layer (HAL)

--- main.c
+++ main.c
@@ -1,21 +1,20 @@
+#include <stdint.h>
 #include "rcc_hal.h"
 #include "gpio_hal.h"
-#include "stm32_def.h"
 
-#include <stdint.h>
-
-
-
-int main(void) {
-
+int main(void)
+{
+    // 1. Enable GPIOA clock
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    // 2. Configure PA5 as output
     GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = 0; // PA0
-    GPIO_InitStruct.Mode = 1; // Output
+    GPIO_InitStruct.Pin = 5; // PA5
+    GPIO_InitStruct.Mode = 1; // Output mode
     GPIO_Init(GPIOA, &GPIO_InitStruct);
+    // 3. Main loop: Toggle PA5
 
-    while (1) {
-        GPIO_TogglePin(GPIOA, 0);
+    while(1) {
+        GPIO_TogglePin(GPIOA, 5); // Toggle PA5
         for (volatile uint32_t i = 0; i < 100000; i++); // Simple delay
-        }
+    }
 }
